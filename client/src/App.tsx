28c0f4be31299b9c7eba074a16346333e83b8672
Mazen Afifi <mazen.afifi@flairstech.com>
2025-02-20 15:51:50 +0200

    Routes Init successfully

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
 
 
 const App = () => {
   return (
-    <div >App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
