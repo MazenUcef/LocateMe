4d381cafb627f3f1827dac060ec6befdc08347da
Mazen Afifi <mazen.afifi@flairstech.com>
2025-02-23 22:17:16 +0200

    Connected to data base successfully

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
 
 
+const mongoUri = process.env.MONGO;
+if (!mongoUri) {
+    throw new Error("MONGO environment variable is not defined");
+}
 
+
+// ✅ Mongoose settings
+mongoose.set("strictQuery", true); // Suppress deprecated warning for strict mode
+mongoose.set("autoIndex", true); // Automatically create indexes
+mongoose.set("bufferCommands", false); // Disable command buffering
+
+mongoose
+    .connect(mongoUri)
+    .then(() => console.log("connected to database successfully"))
+    .catch(() => console.log("failed to connect to database"))
 const app = express();
 
 
